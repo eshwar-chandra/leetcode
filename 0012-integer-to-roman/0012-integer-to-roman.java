fc9a0cf73c37ece540aa30d3150ea9fe7487c46e
Eshwar Chandra Vidya Sagar G <60198123+eshwar-chandra@users.noreply.github.com>
2025-06-24 02:29:41 +0530

    Time: 3 ms (98.39%), Space: 44.3 MB (71.72%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,19 +1,12 @@
 class Solution {
 
-        final static int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        final static String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private final int[] values;
+    private final String[] romanNumerals;
 
-    // public void Solution() {
-    //     this.roman = new HashMap<>();
-    //     roman.put(1, "I");
-    //     roman.put(5, "V");
-    //     roman.put(10, "X");
-    //     roman.put(50, "L");
-    //     roman.put(100, "C");
-    //     roman.put(500, "D");
-    //     roman.put(1000, "M");
-    //     values = {1000, 500, 100, 50, 10, 5, 1};
-    // }
+    public Solution() {
+        this.values = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        this.romanNumerals = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    }
     
     public String intToRoman(int num) {
     
